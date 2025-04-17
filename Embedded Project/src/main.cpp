bb82ad3eb098fd4cad2b0942cee4aadd2ca7049d
kjmo404 <kjm7832@nyu.edu>
2025-04-17 11:24:35 -0400

    delete smth

--- Embedded Project/src/main.cpp
+++ Embedded Project/src/main.cpp
@@ -1,17 +1,14 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(float, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-//feniusebfuoysiegfuyisekhfbsekfvc
-
-//helin was here!!
