9fc53b2461edf1222d5f21bb20d44ea54afba3c6
Helin Mazi <hellinmazi@gmail.com>
2025-04-24 11:21:39 -0400

    fft lib

--- Embedded Project/src/tremor_detection/tremor.cpp
+++ Embedded Project/src/tremor_detection/tremor.cpp
@@ -5,3 +5,28 @@ void detect_tremor(String &label, float &intensity) {
   label = "mild";
   intensity = random(1, 100) / 100.0;
 }
+
+#include <Adafruit_CircuitPlayground.h>
+
+float X, Y, Z;
+
+void acc_setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+}
+
+void getdata() {
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+
+  Serial.print("X: ");
+  Serial.print(X);
+  Serial.print("  Y: ");
+  Serial.print(Y);
+  Serial.print("  Z: ");
+  Serial.println(Z);
+
+  delay(1000);
+}
+
--- Embedded Project/src/tremor_detection/tremor.h
+++ Embedded Project/src/tremor_detection/tremor.h
@@ -2,8 +2,10 @@
 #define TREMOR_H
 
 #include <Arduino.h>
+#include <Adafruit_CircuitPlayground.h>
 
 // Function prototype
 void detect_tremor(String &label, float &intensity);
-
+void getdata();
+void acc_setup();
 #endif
