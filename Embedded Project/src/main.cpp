0572137982b49d81ed3702aeaad2757a8cb54441
KJ Moses <kjm7832@nyu.edu>
2025-04-24 11:16:19 -0400

    initial speaker code

--- Embedded Project/src/main.cpp
+++ Embedded Project/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
-
-// put function declarations here:
-
+#include <Adafruit_CircuitPlayground.h>
 
 void setup() {
-  // put your setup code here, to run once:
-
+  CircuitPlayground.begin();   // initialize the Circuit Playground library
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+   float movementX, movementY, movementZ, movement;
+   uint16_t sound;
+   if(CircuitPlayground.slideSwitch()) {   // sense & play when slide whitch at "+"
+      movementX = abs(CircuitPlayground.motionX());  // read the X motion (absolute value)
+      movementY = abs(CircuitPlayground.motionY());  // read the Y motion (absolute value)
+      movementZ = abs(CircuitPlayground.motionZ());  // read the Z motion (absolute value)
+      movement = movementX + movementY + movementZ;  // aggregate the movement sensed
+      Serial.println(movement);
 
+      if (movement > 10.0) {
+         sound = (int) map(movement, 10.0, 60.0, 440.0, 1760.0);
+         CircuitPlayground.playTone(sound, 500);
+       }
+   }
+}
