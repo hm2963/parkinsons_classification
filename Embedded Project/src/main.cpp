29de2ae68cabb3b0846af669f160da9b8786b0a3
Mathew Ponon <mop9047@nyu.edu>
2025-04-28 11:55:14 -0400

    added 3fft axis code

--- Embedded Project/src/main.cpp
+++ Embedded Project/src/main.cpp
@@ -1,43 +1,162 @@
 #include <Arduino.h>
-#include "tremor_detection/tremor.h"
-#include "led_logic/led.h"
-#include "sound_logic/sound.h"
+#include <Adafruit_CircuitPlayground.h>
+#include "arduinoFFT.h"
 
+
+double findDominantFrequency(double* vReal);
+
+void detect_tremor(String &label, float &intensity) {
+  label = "mild";
+  intensity = random(1, 100) / 100.0;
+}
+
+// Gravity estimation
+float gravityX = 0.0, gravityY = 0.0, gravityZ = 0.0;
+float alpha = 0.8; // smoothing factor for low-pass filter
+
+double X, Y, Z;
+
+const uint16_t samples = 64; // Must be a power of 2
+const double samplingFrequency = 20; // Hz
+
+// Arrays for FFT
+double vRealx[samples], vImagx[samples];
+double vRealy[samples], vImagy[samples];
+double vRealz[samples], vImagz[samples];
+
+// FFT objects
+ArduinoFFT<double> FFTx = ArduinoFFT<double>(vRealx, vImagx, samples, samplingFrequency);
+ArduinoFFT<double> FFTy = ArduinoFFT<double>(vRealy, vImagy, samples, samplingFrequency);
+ArduinoFFT<double> FFTz = ArduinoFFT<double>(vRealz, vImagz, samples, samplingFrequency);
 
 void setup() {
-  initLED();  // put init function from your feature
-  initSound(); // put init function from your feature
-  acc_setup();
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Ready");
+  CircuitPlayground.begin();
+}
+
+double getMotionX() {
+  X = CircuitPlayground.motionX();
+  gravityX = alpha * gravityX + (1 - alpha) * X;
+  return X - gravityX;
+}
+
+double getMotionY() {
+  Y = CircuitPlayground.motionY();
+  gravityY = alpha * gravityY + (1 - alpha) * Y;
+  return Y - gravityY;
+}
+
+double getMotionZ() {
+  Z = CircuitPlayground.motionZ();
+  gravityZ = alpha * gravityZ + (1 - alpha) * Z;
+  return Z - gravityZ;
 }
 
 void loop() {
-  String type;
-  float intensity;
-
-  getdata();
-
-  detect_tremor(type, intensity);  // modifies type & intensity by reference
-  Serial.println("Tremor: " + type + ", intensity: " + String(intensity));
-
-  if(type == "normal"){
-    handleLEDN(intensity);
-    handleSoundN(intensity);
-  } 
-
-  else if(type == "tremor"){
-    handleLEDTM(intensity);
-    handleSoundTM(intensity);
-  } 
-  
-  else if (type == "dyskinesia") {
-    handleLEDDK(intensity);
-    handleSoundDK(intensity);
+  // Collect samples
+  for (uint16_t i = 0; i < samples; i++) {
+    vRealx[i] = getMotionX();
+    vImagx[i] = 0.0;
+    vRealy[i] = getMotionY();
+    vImagy[i] = 0.0;
+    vRealz[i] = getMotionZ();
+    vImagz[i] = 0.0;
+    delay(1000.0 / samplingFrequency);
   }
 
-  //default value, meaning didnt change variable or undefined
-  else { 
-    // must have logic for if detection did not happen
-  }
+  // Process FFT for X
+  FFTx.dcRemoval();
+  FFTx.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FFTx.compute(FFTDirection::Forward);
+  FFTx.complexToMagnitude();
 
-  delay(1000);
+  // Process FFT for Y
+  FFTy.dcRemoval();
+  FFTy.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FFTy.compute(FFTDirection::Forward);
+  FFTy.complexToMagnitude();
+
+  // Process FFT for Z
+  FFTz.dcRemoval();
+  FFTz.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FFTz.compute(FFTDirection::Forward);
+  FFTz.complexToMagnitude();
+
+  // Find dominant frequency for Z (you can also do for X and Y if you want)
+  double dominantFreqZ = findDominantFrequency(vRealz);
+
+  Serial.print("Dominant Frequency (Z-axis): ");
+  Serial.print(dominantFreqZ, 2);
+  Serial.println(" Hz");
+
+  double dominantFreqX = findDominantFrequency(vRealx);
+
+  Serial.print("Dominant Frequency (X-axis): ");
+  Serial.print(dominantFreqX, 2);
+  Serial.println(" Hz");
+
+  double dominantFreqY = findDominantFrequency(vRealy);
+
+  Serial.print("Dominant Frequency (Y-axis): ");
+  Serial.print(dominantFreqY, 2);
+  Serial.println(" Hz");
 }
+
+double findDominantFrequency(double *vReal) {
+  double maxMagnitude = 0;
+  uint16_t maxIndex = 0;
+  for (uint16_t i = 1; i < (samples / 2); i++) { // skip DC component
+    if (vReal[i] > maxMagnitude) {
+      maxMagnitude = vReal[i];
+      maxIndex = i;
+    }
+  }
+  return (maxIndex * 1.0 * samplingFrequency) / samples;
+}
+
+
+// #include <Arduino.h>
+// #include "tremor_detection/tremor.h"
+// #include "led_logic/led.h"
+// #include "sound_logic/sound.h"
+
+
+// void setup() {
+//   initLED();  // put init function from your feature
+//   initSound(); // put init function from your feature
+//   acc_setup();
+// }
+
+// void loop() {
+//   String type;
+//   float intensity;
+
+//   getdata();
+
+//   detect_tremor(type, intensity);  // modifies type & intensity by reference
+//   Serial.println("Tremor: " + type + ", intensity: " + String(intensity));
+
+//   if(type == "normal"){
+//     handleLEDN(intensity);
+//     handleSoundN(intensity);
+//   } 
+
+//   else if(type == "tremor"){
+//     handleLEDTM(intensity);
+//     handleSoundTM(intensity);
+//   } 
+  
+//   else if (type == "dyskinesia") {
+//     handleLEDDK(intensity);
+//     handleSoundDK(intensity);
+//   }
+
+//   //default value, meaning didnt change variable or undefined
+//   else { 
+//     // must have logic for if detection did not happen
+//   }
+
+//   delay(1000);
+// }
--- Embedded Project/src/tremor_detection/tremor.cpp
+++ Embedded Project/src/tremor_detection/tremor.cpp
@@ -1,4 +1,6 @@
 #include "tremor.h"
+#include <Adafruit_CircuitPlayground.h>
+#include "arduinoFFT.h"
 
 void detect_tremor(String &label, float &intensity) {
   // Mock example, replace with sensor logic
@@ -6,27 +8,102 @@ void detect_tremor(String &label, float &intensity) {
   intensity = random(1, 100) / 100.0;
 }
 
-#include <Adafruit_CircuitPlayground.h>
+
 
 float X, Y, Z;
 
 void acc_setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Ready");
   CircuitPlayground.begin();
 }
 
-void getdata() {
+float getdataX(){
   X = CircuitPlayground.motionX();
-  Y = CircuitPlayground.motionY();
-  Z = CircuitPlayground.motionZ();
-
   Serial.print("X: ");
   Serial.print(X);
+  return X;
+}
+
+float getdataY(){
+  Y = CircuitPlayground.motionY();
   Serial.print("  Y: ");
   Serial.print(Y);
+  return Y;
+}
+
+float getdataZ() {
+  Z = CircuitPlayground.motionZ();
   Serial.print("  Z: ");
   Serial.println(Z);
-
-  delay(1000);
+  return Z;
 }
+
+const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
+// const double signalFrequency = 1000;
+const double samplingFrequency = 5000;
+// const uint8_t amplitude = 100;
+
+float vReal[samples];
+float vImag[samples];
+
+ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency);
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
+
+
+void FFTify(float P)
+{
+  /* Build raw data */
+  // double ratio = twoPi * signalFrequency / samplingFrequency; // Fraction of a complete cycle stored at each sample (in radians)
+  for (uint16_t i = 0; i < samples; i++)
+  {
+    vReal[i] = getdataX();
+    // vReal[i] = int8_t(amplitude * sin(i) / 2.0);/* Build data with positive and negative values*/
+    //vReal[i] = uint8_t((amplitude * (sin(i * ratio) + 1.0)) / 2.0);/* Build data displaced on the Y axis to include only positive values*/
+    vImag[i] = 0.0; //Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
+  }
+  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
+  FFT.compute(FFTDirection::Forward); /* Compute FFT */
+  FFT.complexToMagnitude(); /* Compute magnitudes */
+  PrintVector(vReal, samples>>1, SCL_PLOT);
+  double x = FFT.majorPeak();
+  // while(1); /* Run Once */
+  // delay(2000); /* Repeat after delay */
+}
+
+void PrintVector(float *vData, uint16_t bufferSize, uint8_t scaleType)
+{
+  for (uint16_t i = 0; i < bufferSize; i++)
+  {
+    double abscissa;
+    /* Print abscissa value */
+    switch (scaleType)
+    {
+      case SCL_INDEX:
+        abscissa = (i * 1.0);
+	      break;
+      case SCL_TIME:
+        abscissa = ((i * 1.0) / samplingFrequency);
+        break;
+      case SCL_FREQUENCY:
+        abscissa = ((i * 1.0 * samplingFrequency) / samples);
+	      break;
+    }
+    if(scaleType!=SCL_PLOT)
+    {
+      Serial.print(abscissa, 6);
+      if(scaleType==SCL_FREQUENCY)
+        Serial.print("Hz");
+      Serial.print(" ");
+    }
+    Serial.println(vData[i], 4);
+  }
+  Serial.println();
+}
+
 
